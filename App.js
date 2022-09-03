912202513be0ae091f337aca2d8735586a123ad2
Ahmed-Basuony <ahmed.basouny@awstreams.com>
2022-09-04 00:39:11 +0200

    Add FlatList to make list more effecient

--- App.js
+++ App.js
@@ -1,30 +1,48 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
-
-  const [enteredText, setEnteredText] = useState('');
+  const [enteredText, setEnteredText] = useState("");
   const [goalList, setGoalList] = useState([]);
 
   function inputTextHandler(text) {
-    setEnteredText(text)
+    setEnteredText(text);
   }
 
-
   function addGoalHandler() {
-   setGoalList((currGoalList) => [...currGoalList, enteredText])
+    setGoalList((currGoalList) => [
+      ...currGoalList,
+      { text: enteredText, key: Math.random().toString() },
+    ]);
   }
 
   return (
     <View style={styles.container}>
-      <View  style={styles.inputContainer} >
-        <TextInput placeholder='Your course goal...' style={styles.textInput} onChangeText={inputTextHandler}/>
-        <Button title='Add goal' onPress={addGoalHandler}/>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Your course goal..."
+          style={styles.textInput}
+          onChangeText={inputTextHandler}
+        />
+        <Button title="Add goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.listConatiner}>
-        {goalList.map((goal) => <View  key={goal} style={styles.listItem}>
-          <Text style={styles.goalText}>{goal}</Text>
-        </View>)}
+        <FlatList
+          data={goalList}
+          renderItem={(itemData) => (
+            <View key={goal} style={styles.listItem}>
+              <Text style={styles.goalText}>{itemData.item}</Text>
+            </View>
+          )}
+        />
       </View>
     </View>
   );
@@ -33,41 +51,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     // alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingTop: 50,
     paddingHorizontal: 16,
   },
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginBottom: 20,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1
-  }, 
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
   textInput: {
-    borderColor: '#cccccc',
+    borderColor: "#cccccc",
     borderWidth: 1,
-    width: '70%',
+    width: "70%",
     marginRight: 8,
-    padding: 4
-  },  
+    padding: 4,
+  },
   listConatiner: {
     flex: 4,
-  }, 
+  },
   listItem: {
     marginVertical: 8,
     padding: 8,
-    borderWidth: 1, 
-    borderColor: '#eee',
+    borderWidth: 1,
+    borderColor: "#eee",
     borderRadius: 10,
-    backgroundColor: '#A460ED',
+    backgroundColor: "#A460ED",
   },
   goalText: {
-    color: '#EEEEEE'
-    
-  }
+    color: "#EEEEEE",
+  },
 });
